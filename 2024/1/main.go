5e1e2719fd53b88c23a27b5c9e9c2709c5c40b0d
Timur Gaitov <timur.gaitov@gmail.com>
2024-12-01 14:31:38 +0700

    day 1 part 2

--- 2024/1/main.go
+++ 2024/1/main.go
@@ -14,11 +14,28 @@ func main() {
 	if len(left) != len(right) {
 		panic("lengths do not match")
 	}
+	// part 1
 	sum := 0
 	for i := 0; i < len(left); i++ {
 		sum += distance(left[i], right[i])
 	}
 	fmt.Println(sum)
+	// part 2
+	sim := 0
+	for l, r := 0, 0; l < len(left) && r < len(right); {
+		countL := 0
+		countR := 0
+		cur := left[l]
+		// skip smaller numbers on the right
+		for ; r < len(right) && right[r] < cur; r++ {
+		}
+		for ; l < len(left) && left[l] == cur; countL, l = countL+1, l+1 {
+		}
+		for ; r < len(right) && right[r] == cur; countR, r = countR+1, r+1 {
+		}
+		sim += cur * countL * countR
+	}
+	fmt.Println(sim)
 }
 
 func distance(a, b int) int {
