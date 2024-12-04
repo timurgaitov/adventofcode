a64da55e2fba3f233f87e5719eb277037eabdeef
Timur Gaitov <timur.gaitov@gmail.com>
2024-12-04 15:33:10 +0700

    day 4 part 2

--- 2024/4/main.go
+++ 2024/4/main.go
@@ -12,46 +12,66 @@ func main() {
 	}
 	size := len(M)
 
-	res := 0
-	for x := 0; x < size; x++ {
-		for y := 0; y < size; y++ {
+	res1 := 0
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
 			for _, d := range dirs {
-				res += searchXMAS(M, x, y, size, 0, d)
+				res1 += searchXMAS(M, i, j, size, 0, d)
 			}
 		}
 	}
-	fmt.Println(res)
+	fmt.Println(res1)
+
+	res2 := 0
+	for i := 1; i < size-1; i++ {
+		for j := 1; j < size-1; j++ {
+			res2 += searchMASCROSS(M, i, j)
+		}
+	}
+	fmt.Println(res2)
 }
 
 type dir struct {
-	x int
-	y int
+	i int
+	j int
 }
 
 var dirs = []dir{
-	{x: 0, y: 1},
-	{x: 1, y: 1},
-	{x: 1, y: 0},
-	{x: 1, y: -1},
-	{x: 0, y: -1},
-	{x: -1, y: -1},
-	{x: -1, y: 0},
-	{x: -1, y: 1},
+	{i: 0, j: 1},
+	{i: 1, j: 1},
+	{i: 1, j: 0},
+	{i: 1, j: -1},
+	{i: 0, j: -1},
+	{i: -1, j: -1},
+	{i: -1, j: 0},
+	{i: -1, j: 1},
 }
 
-func searchXMAS(M []string, x, y, size int, cur int, d dir) int {
-	if isOutOfBoundaries(x, y, size) {
+func searchXMAS(M []string, i, j, size int, cur int, d dir) int {
+	if isOutOfBoundaries(i, j, size) {
 		return 0
 	}
-	if M[x][y] != "XMAS"[cur] {
+	if M[i][j] != "XMAS"[cur] {
 		return 0
 	}
 	if cur == len("XMAS")-1 {
 		return 1
 	}
-	return searchXMAS(M, x+d.x, y+d.y, size, cur+1, d)
+	return searchXMAS(M, i+d.i, j+d.j, size, cur+1, d)
 }
 
-func isOutOfBoundaries(x, y int, size int) bool {
-	return x < 0 || x >= size || y < 0 || y >= size
+func searchMASCROSS(M []string, i, j int) int {
+	if M[i][j] != 'A' {
+		return 0
+	}
+	diag1 := fmt.Sprintf("%cA%c", M[i-1][j-1], M[i+1][j+1])
+	diag2 := fmt.Sprintf("%cA%c", M[i+1][j-1], M[i-1][j+1])
+	if diag1 != "MAS" && diag1 != "SAM" || diag2 != "MAS" && diag2 != "SAM" {
+		return 0
+	}
+	return 1
+}
+
+func isOutOfBoundaries(i, j int, size int) bool {
+	return i < 0 || i >= size || j < 0 || j >= size
 }
