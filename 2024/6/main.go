d85a2b0939954f298566ddeca1df91df3c0d082a
Timur Gaitov <timur.gaitov@gmail.com>
2024-12-06 16:31:15 +0700

    cleanup

--- 2024/6/main.go
+++ 2024/6/main.go
@@ -34,7 +34,6 @@ func main() {
 	dir1 := 0
 	for i, j := guardI, guardJ; !outOfMap(i, j, size); {
 		if M[i][j] == '#' {
-			// obstacle, go back, change direction
 			i, j = i-dirs[dir1].i, j-dirs[dir1].j
 			dir1 = (dir1 + 1) % 4
 			continue
@@ -53,13 +52,11 @@ func main() {
 			prevDir2 := 0
 			for i, j := guardI, guardJ; !outOfMap(i, j, size); {
 				if M[i][j] == '#' || i == obsI && j == obsJ {
-					// obstacle, go back, change direction
 					i, j = i-dirs[dir2].i, j-dirs[dir2].j
 					dir2 = (dir2 + 1) % 4
 					continue
 				}
 				if dir2 != prevDir2 {
-					// turned
 					prevDir2 = dir2
 					tur := turn{i: i, j: j, dir: dirs[dir2]}
 					if _, ok := visitedTurns[tur]; ok {
