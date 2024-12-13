53ebce763271a12d8bc021a4d3289d0c2e8eab02
Timur Gaitov <timur.gaitov@gmail.com>
2024-12-13 23:11:25 +0700

    day 13 part 2 py

--- 2024/13/main.py
+++ 2024/13/main.py
@@ -13,17 +13,25 @@ with open("input.txt") as file:
     p = [int(x.split("=")[1]) for x in lines[i].split(": ")[1].split(", ")]
     i += 1
     i += 1
+    
+    # part 2
+    p[0] += 10000000000000
+    p[1] += 10000000000000
+    
     A = np.array([[a[0], b[0]], [a[1], b[1]]])
     B = np.array([p[0], p[1]])
     x = np.linalg.solve(A, B)
-    x[0] = np.round(x[0], 1)
-    x[1] = np.round(x[1], 1)
+    x[0] = np.round(x[0], 2)
+    x[1] = np.round(x[1], 2)
     if x[0] < 0 or x[1] < 0:
       continue
     if x[0] % 1 > 0 or x[1] % 1 > 0:
       continue
-    if x[0] > 100 or x[1] > 100:
-      continue
+      
+    # part 2
+    # if x[0] > 100 or x[1] > 100:
+    #   continue
+    
     res += 3*x[0]+x[1]
     
   print(res)
