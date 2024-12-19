bb874f09f47be6749c57a9b8e9555fa1b03fbcd8
Timur Gaitov <timur.gaitov@gmail.com>
2024-12-19 15:42:47 +0700

    day 18 part 1

---
+++ 2024/18/main.py
@@ -0,0 +1,55 @@
+from collections import defaultdict
+from heapq import heappush, heappop
+from math import inf
+
+with open('input.txt') as file:
+  blocks = [(int(c[0]), int(c[1])) for c in [str_c.rstrip().split(',') for str_c
+                                             in file.readlines()]]
+
+# size = 7
+size = 71
+# fallen = 12
+fallen = 1024
+
+space = [[0 for x in range(size)] for y in range(size)]
+
+for b in blocks[:fallen]:
+  if b[0] < 0 or b[0] >= size or b[1] < 0 or b[1] >= size:
+    continue
+
+  space[b[1]][b[0]] = 1
+
+dirs = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+
+
+def mov(pos, d):
+  return pos[0] + d[0], pos[1] + d[1]
+
+
+def find_path(start):
+  dist = defaultdict(lambda: inf)
+
+  pq = []
+  heappush(pq, (0, start))
+
+  while len(pq) > 0:
+    cost, cur = heappop(pq)
+
+    if cur[0] < 0 or cur[0] >= size or cur[1] < 0 or cur[1] >= size:
+      continue
+
+    if space[cur[0]][cur[1]] == 1:
+      continue
+
+    if cost >= dist[cur]:
+      continue
+    dist[cur] = cost
+
+    for d in dirs:
+      heappush(pq, (dist[cur] + 1, mov(cur, d)))
+
+  return dist
+
+
+dist0 = find_path((0, 0))
+print(dist0[(size - 1, size - 1)])
