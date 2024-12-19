eeab8e869ddc22a4d2baf3c6dda3555e98c34626
Timur Gaitov <timur.gaitov@gmail.com>
2024-12-19 15:02:04 +0700

    day 17 part 2

--- 2024/17/main.py
+++ 2024/17/main.py
@@ -5,70 +5,60 @@ A = int(lines[0].split(': ')[1])
 B = int(lines[1].split(': ')[1])
 C = int(lines[2].split(': ')[1])
 prog = [int(x) for x in lines[4].split(': ')[1].split(',')]
-pos = 0
-output = []
 
 
-def combo(op):
+def combo(op, proc):
   assert 0 <= op < 7
   if op < 4:
     return op
   if op == 4:
-    return A
+    return proc[0]
   if op == 5:
-    return B
+    return proc[1]
   if op == 6:
-    return C
+    return proc[2]
 
 
-def adv(op):
-  global A, pos
-  A //= 2 ** combo(op)
-  pos += 2
+def adv(op, proc):
+  proc[0] >>= combo(op, proc)
+  proc[3] += 2
 
 
-def bxl(op):
-  global B, pos
-  B ^= op
-  pos += 2
+def bxl(op, proc):
+  proc[1] ^= op
+  proc[3] += 2
 
 
-def bst(op):
-  global B, pos
-  B = combo(op) % 8
-  pos += 2
+def bst(op, proc):
+  proc[1] = combo(op, proc) & 0b111
+  proc[3] += 2
 
 
-def jnz(op):
-  global A, pos
-  if A == 0:
-    pos += 2
+def jnz(op, proc):
+  if proc[0] == 0:
+    proc[3] += 2
     return
-  pos = op
+  proc[3] = op
 
 
-def bxc(_):
-  global B, C, pos
-  B ^= C
-  pos += 2
+def bxc(_, proc):
+  proc[1] ^= proc[2]
+  proc[3] += 2
 
 
-def out(op):
-  global output, pos
-  output.append(combo(op) % 8)
-  pos += 2
+def out(op, proc):
+  proc[4].append(combo(op, proc) & 0b111)
+  proc[3] += 2
 
 
-def bdv(op):
-  global A, B, pos
-  B = A // 2 ** combo(op)
-  pos += 2
+def bdv(op, proc):
+  proc[1] = proc[0] >> combo(op, proc)
+  proc[3] += 2
 
 
-def cdv(op):
-  global A, C, pos
-  C = A // 2 ** combo(op)
-  pos += 2
+def cdv(op, proc):
+  proc[2] = proc[0] >> combo(op, proc)
+  proc[3] += 2
 
 
 opcode = [
@@ -82,8 +72,30 @@ opcode = [
   cdv,
 ]
 
-while pos < len(prog):
-  opcode[prog[pos]](prog[pos + 1])
-  print('', end='')
+proc = [A, B, C, 0, []]
+while proc[3] < len(prog):
+  opcode[prog[proc[3]]](prog[proc[3] + 1], proc)
 
-print(','.join([str(x) for x in output]))
+print(','.join([str(x) for x in proc[4]]))
+
+
+# part 2
+# guessing three bits erased by modulo
+def find_a(backw_i, a):
+  if backw_i < 0:
+    return a
+  for x in range(0b111 + 1):
+    cand_a = a << 3 | x
+
+    proc = [cand_a, 0, 0, 0, []]
+    for i in range(0, len(prog), 2):
+      opcode[prog[i]](prog[i + 1], proc)
+
+    if proc[4][0] == prog[backw_i]:
+      result = find_a(backw_i - 1, cand_a)
+      if result is None:
+        continue
+      return result
+
+
+print(find_a(len(prog) - 1, 0))
