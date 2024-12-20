3d85e25dd60b4447f7d7a18c3b4006baa7e9631a
Timur Gaitov <timur.gaitov@gmail.com>
2024-12-21 00:36:31 +0700

    day 20 part 2

--- 2024/20/main.py
+++ 2024/20/main.py
@@ -4,19 +4,19 @@ from heapq import heappush, heappop
 from math import inf
 
 with open('input.txt') as file:
-  track = [list(x) for x in file.read().splitlines()]
+  Map = [list(x) for x in file.read().splitlines()]
 
-size = len(track)
+size = len(Map)
 for i, j in itertools.product(range(size), repeat=2):
-  if track[i][j] == 'E':
+  if Map[i][j] == 'E':
     E = (i, j)
-  elif track[i][j] == 'S':
+  elif Map[i][j] == 'S':
     S = (i, j)
 
-dirs = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+dirs = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
 
-def mov(pos, d):
+def move(pos, d):
   return pos[0] + d[0], pos[1] + d[1]
 
 
@@ -37,41 +37,35 @@ def find_path(_map, _start):
     _dist[cur] = cost
 
     for d in dirs:
-      heappush(pq, (_dist[cur] + 1, mov(cur, d)))
+      heappush(pq, (_dist[cur] + 1, move(cur, d)))
 
   return _dist
 
 
-dist_E_to_others = find_path(track, E)
-dist_S_to_others = find_path(track, S)
+dist_e = find_path(Map, E)
+dist_s = find_path(Map, S)
 
-count = 0
+save = 100
+cheats = 20
+ref_val = dist_s[E]
 
-for i, j in itertools.product(range(1, size - 1), repeat=2):
-  if track[i][j] != '#':
+count = set()
+
+for i, j, k, m in itertools.product(range(0, size), repeat=4):
+  if Map[i][j] == '#' or Map[k][m] == '#':
     continue
 
-  left = mov((i, j), dirs[2])
-  right = mov((i, j), dirs[0])
-  top = mov((i, j), dirs[1])
-  bottom = mov((i, j), dirs[3])
+  d = abs(i - k) + abs(j - m)
 
-  saved = 100
+  if d == 0:
+    continue
 
-  if dist_E_to_others[left] + dist_S_to_others[right] + 2 <= dist_S_to_others[
-    E] - saved:
-    count += 1
+  if d > cheats:
+    continue
 
-  if dist_E_to_others[right] + dist_S_to_others[left] + 2 <= dist_S_to_others[
-    E] - saved:
-    count += 1
+  if dist_s[(i, j)] + dist_e[(k, m)] + d > ref_val - save:
+    continue
 
-  if dist_E_to_others[top] + dist_S_to_others[bottom] + 2 <= dist_S_to_others[
-    E] - saved:
-    count += 1
+  count.add(((i, j), (k, m)))
 
-  if dist_E_to_others[bottom] + dist_S_to_others[top] + 2 <= dist_S_to_others[
-    E] - saved:
-    count += 1
-
-print(count)
+print(len(count))
