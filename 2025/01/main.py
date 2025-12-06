33a15bb6de57dc8df45a884d65543c0f9fafe000
Timur Gaitov <timur.gaitov@gmail.com>
2025-12-06 23:30:36 +0700

    day 1 part 2

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -8,11 +8,17 @@ with open(sys.argv[1], 'r') as file:
 		direct = line[0]
 		assert direct in ('L', 'R')
 		num = int(line[1:])
+		rot = num // 100
+		num %= 100
+		before = x
 		if direct == 'L':
 			x = (x - num + 100) % 100
+			if x == 0 or (before != 0 and x > before):
+				rot += 1
 		elif direct == 'R':
 			x = (x + num) % 100
-		if x == 0:
-			cnt += 1
+			if x < before:
+				rot += 1
+		cnt += rot
 	print(cnt)	
 
